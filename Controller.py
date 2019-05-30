c00a67530b53ea14b23b3cd7e35a03fdea8f7121
Deepam Gupta <deepam.deepam.gupta@gmail.com>
2019-05-30 14:19:11 +0530

    finished Register Model, fixed the bug of jquery, scripty.js

--- Controller.py
+++ Controller.py
@@ -1,14 +1,19 @@
 # all the top level code
 
 # web.py
+
+
 import web
+from Models import RegisterModel
+
 
 urls = (
-    '/', 'home',
-    '/register', 'register'
+    '/', 'Home',
+    '/register', 'Register',
+    '/postregistration', 'PostRegistration'
 )
 
-render =  web.template.render("Views/Templates", base="MainLayout")
+render = web.template.render("Views/Templates", base="MainLayout")
 
 app = web.application(urls, globals())
 
@@ -16,13 +21,22 @@ app = web.application(urls, globals())
 # Classes/Routes
 # Each class will be controlling a route
 
-class home:
+class Home:
     def GET(self):
         return render.Home()
 
-class register:
+class Register:
     def GET(self):
         return render.Register()
+
+class PostRegistration:
+    def POST(self):
+        data = web.input()
+        print(data.username)
+
+        reg_model = RegisterModel.RegisterModel()
+        reg_model.insert_user(data)
+        return data.username
 
 
 if __name__=="__main__":
--- static/js/scripty.js
+++ static/js/scripty.js
@@ -1,10 +1,25 @@
 
-$(document).ready(function () {
-    console.log("loaded");
-    $.material.init();
+    $(document).ready(function () {
+        console.log("loaded");
 
-    $(document).on("submit", "#register-form", function (e) {
-        e.preventDefault();
-        console.log("form submitted");
+        if (typeof $.material !== 'undefined') {
+            $.material.init();
+        }
+
+        $(document).on("submit", "#register-form", function (e) {
+            e.preventDefault();
+            console.log("form submitted");
+
+            let form = $('#register-form').serialize(); //get the form data
+
+            //send an ajax request over to the route /postregisteration
+            $.ajax({
+                url: '/postregistration',
+                type: 'POST',
+                data: form,
+                success: function (response) {
+                    console.log(response);
+                }
+            });
+        });
     });
-});
